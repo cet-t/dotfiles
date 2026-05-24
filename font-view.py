2ee803ef8e8b13befe633ce3ebe4bbd5d4ff5a67
cet <cets021@gmail.com>
2026-05-24 23:48:41 +0900

    add packed font

---
+++ font-view.py
@@ -0,0 +1,13 @@
+def add(a: int, b: int) -> int:
+    """a と b の足し算"""
+    return a + b
+
+
+def main() -> None:
+    a = 12
+    b = 24
+    _ = add(a, b)
+
+
+if __name__ == "__main__":
+    main()
