fc76ae620351cf80ff611748cb730a37182322da
cet <cets021@gmail.com>
2026-06-27 17:58:13 +0900

    feat(nvim): replace telescope with fzf-lua

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -1,21 +1,17 @@
 return {
 	{
-		"nvim-telescope/telescope.nvim",
-		branch = "0.1.x",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		"ibhagwan/fzf-lua",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		keys = {
+			{ "<leader>ff", "<cmd>FzfLua files<CR>", desc = "Find files" },
+			{ "<leader>fg", "<cmd>FzfLua live_grep<CR>", desc = "Live grep" },
+			{ "<leader>fb", "<cmd>FzfLua buffers<CR>", desc = "Buffers" },
+			{ "<leader>fh", "<cmd>FzfLua help_tags<CR>", desc = "Help tags" },
 		},
-		config = function()
-			local telescope = require("telescope")
-			telescope.setup({
-				defaults = {
-					path_display = { "truncate" },
-					preview = { treesitter = false },
-				},
-			})
-			pcall(telescope.load_extension, "fzf")
-		end,
+		opts = {
+			defaults = { file_icons = true },
+			winopts = { preview = { default = "bat" } },
+		},
 	},
 
 	{
